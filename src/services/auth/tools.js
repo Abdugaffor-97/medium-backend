066b410319aabfb040a1900c4b719305b98267bf
Abdugaffor <abdurahimov.97@list.ru>
2021-02-18 11:01:54 +0500

    Updated

--- src/services/auth/tools.js
+++ src/services/auth/tools.js
@@ -1,7 +1,47 @@
 const jwt = require("jsonwebtoken");
 
+
+
+const generateJWT = (payload) => {
+  new Promise((res, rej) => {
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 6 },
+      (err, token) => {
+        if (err) rej(err)
+        res(token)
+      }
+    )
+  })
+}
+
+
+const generateRefreshJWT = (payload) => {
+  new Promise((res, rej) => {
+    jwt.sign(
+      payload,
+      process.env.REFRESH_JWT_SECRET,
+      { expiresIn: "1 week" },
+      (err, token) => {
+        if (err) rej(err)
+        res(token)
+      }
+    )
+  })
+}
+
+
 const authenticate = async (user) => {
   try {
+    const newAccessToken = await generateJWT({ _id: user._id })
+    const newRefreshToken = await generateRefreshJWT({ _id: user._id })
+
+    user.refreshTokens = user.refreshTokens.concat({ token: newRefreshToken })
+    
+    await user.save()
+
+    return { token: newAccessToken, refreshToken: newRefreshToken}
   } catch (error) {}
 };
 
@@ -14,4 +54,4 @@ const verifyJWT = (token) => {
   });
 };
 
-module.exports = { verifyJWT };
+module.exports = { verifyJWT, authenticate };
--- src/services/users/schema.js
+++ src/services/users/schema.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   surname: String,
   email: String,
-  password: String,
+  password: { type: String, required: true },
   articles: [{ type: Schema.Types.ObjectId, ref: "Article" }],
 });
 
